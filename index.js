db9ccf32dbd091d54966a6a606951bde943594b0
Shaheen Ghiassy <shaheen.ghiassy@gmail.com>
2019-06-25 12:17:52 -0700

    Basic setup (#1)
    * Add gitignore
    
    * Add package.json
    
    * Add basic files
    
    * Add ci badge

---
+++ index.js
@@ -0,0 +1,19 @@
+const express = require('express')
+const app = express()
+const logger = require('morgan')
+// const path = require('path')
+// const serveStatic = require('serve-static')
+
+app.use(logger('dev'))
+
+app.get('/', (req, res) => {
+    res.send('Hello World from Heroku')
+})
+
+// app.use(serveStatic(path.join(__dirname, '/build'), {
+//     maxAge: '1d'
+// }))
+
+const port = process.env.PORT || 9000;
+
+app.listen(port, () => console.log(`Listening on port ${port}!`))
