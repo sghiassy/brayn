38b25f25598805c11f8f1fbbf612177dcd1ac6c7
Shaheen Ghiassy <shaheen@groupon.com>
2019-06-26 09:33:04 -0700

    Added node list page

---
+++ api/controllers/view-nodelist.js
@@ -0,0 +1,27 @@
+module.exports = {
+
+
+  friendlyName: 'View nodelist',
+
+
+  description: 'Display "Nodelist" page.',
+
+
+  exits: {
+
+    success: {
+      viewTemplatePath: 'pages/nodelist'
+    }
+
+  },
+
+
+  fn: async function () {
+
+    // Respond with view.
+    return {};
+
+  }
+
+
+};
