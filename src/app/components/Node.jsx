94e9d817699e23ed17eb841604c71a8dc1a8d53a
Shaheen Ghiassy <shaheen@groupon.com>
2019-06-25 23:35:38 -0700

    Added delete button

--- src/app/components/Node.jsx
+++ src/app/components/Node.jsx
@@ -5,15 +5,42 @@ class Node extends Component {
     super(props)
 
     this.state = {
-      name:props.name || "-"
+      node:props.node
     }
   }
 
   render() {
     return (
       <div className="node">
-        <h1>Node {this.state.name}</h1>
+        <h1>Node {this.state.node.name}</h1><DeleteButton onClick={()=>{
+          var xhr = new XMLHttpRequest()
+
+          xhr.open("DELETE", "/v0/nodes/" + this.state.node.id)
+          xhr.setRequestHeader("Content-Type", "application/json;charset=UTF-8");
+          xhr.send()
+        }} />
       </div>
+    );
+  }
+}
+
+class DeleteButton extends Component {
+  constructor(props) {
+    super(props)
+
+    // This binding is necessary to make `this` work in the callback
+    this.handleClick = this.handleClick.bind(this);
+  }
+
+  handleClick() {
+    this.props.onClick()
+  }
+
+  render() {
+    return (
+      <button onClick={this.handleClick}>
+        "Delete"
+      </button>
     );
   }
 }
