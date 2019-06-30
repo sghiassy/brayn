9a35162fbd13140ab0db35ae6e642928c2d5702f
Shaheen Ghiassy <shaheen@groupon.com>
2019-06-30 08:43:47 -0700

    Cleaning

--- api/controllers/NodesController.js
+++ api/controllers/NodesController.js
@@ -1,22 +1,26 @@
+const Nodes = require('../models/Nodes')
 
 module.exports = {
   get: async function (req, res) {
-    // eslint-disable-next-line no-undef
     let nodes = await Nodes.get()
     res.send(nodes)
   },
-  post: function (req, res) {
+
+  post: async function (req, res) {
     if (req.body.name == undefined) {
       return res.send('Name was undefined in body:' + JSON.stringify(req.body))
     }
 
-
+    let newNode = await Nodes.create(req.body.name)
+    res.send(newNode)
   },
 
-  delete: function (req, res) {
+  delete: async function (req, res) {
     if (req.params.id == undefined) {
       return res.send('id is undefined:' + JSON.stringify(req.params))
     }
 
+    let deleted = await Nodes.delete(req.params.id)
+    res.send(deleted)
   }
 }
