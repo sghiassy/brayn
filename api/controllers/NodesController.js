adb357d1e4319dad4aa1696d6061d58bcaaa2c94
Shaheen Ghiassy <shaheen@groupon.com>
2019-06-30 08:09:11 -0700

    Pulling data from GraphDB

--- api/controllers/NodesController.js
+++ api/controllers/NodesController.js
@@ -1,14 +1,49 @@
+global.neo4j = require('neo4j-driver').v1
+
+const graphenedbURL = process.env.GRAPHENEDB_BOLT_URL || 'bolt://localhost:7687'
+const graphenedbUser = process.env.GRAPHENEDB_BOLT_USER || 'neo4j'
+const graphenedbPass = process.env.GRAPHENEDB_BOLT_PASSWORD || 'jolt39'
+
+const driver = global.neo4j.driver(graphenedbURL, global.neo4j.auth.basic(graphenedbUser, graphenedbPass))
+const session = driver.session()
+
 module.exports = {
   get: function (req, res) {
-    res.send('get')
+    session
+      .run('MATCH (n) RETURN { id: ID(n), name: n.name } LIMIT 25')
+      .then((result) => {
+        res.send(result)
+      })
+      .catch((error) => {
+        res.send(error)
+      })
   },
   post: function (req, res) {
-    res.send('post')
+    if (req.body.name == undefined) {
+      return res.send('Name was undefined in body:' + JSON.stringify(req.body))
+    }
+
+    session
+      .run('CREATE (n:Person { name: \'' + req.body.name + '\', title: \'Developer\' })')
+      .then((result) => {
+        res.send(result)
+      })
+      .catch((error) => {
+        res.send(error)
+      })
   },
-  put: function (req, res) {
-    res.send('put')
-  },
+
   delete: function (req, res) {
-    res.send('delete')
+    if (req.params.id == undefined) {
+      return res.send('id is undefined:' + JSON.stringify(req.params))
+    }
+    session
+      .run('MATCH (n) where id(n) = ' + req.params.id + ' DETACH DELETE n')
+      .then((result) => {
+        res.send(result)
+      })
+      .catch((error) => {
+        res.send(error)
+      })
   }
 }
